09aa2ad9d3a6ad96c789f5bc5d929fbbc6743179
harshireddy14 <harshitha.medapati@gmail.com>
2025-12-23 13:56:55 +0530

    Main calculation done

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator{
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Addition" + (a+b));
+        System.out.println("Multiplication" + (a * b));
+        
+    }
+}
